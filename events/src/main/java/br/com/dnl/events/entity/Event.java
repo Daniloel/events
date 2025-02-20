c9390a541ce4e91253cfadeb1a1cad2f353a3014
Danilo <10500114@edp.com.br>
2025-02-20 20:37:18 -0300

    dnl -Mapeada a tabela de event do banco para o package entity

---
+++ events/src/main/java/br/com/dnl/events/entity/Event.java
@@ -0,0 +1,117 @@
+package br.com.dnl.events.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+@Entity
+@Table(name = "tbl_event")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// auto gera o ID
+    @Column(name = "event_id")
+    private Long eventId;
+
+    @Column(name = "title" , length = 255, nullable = false)
+    private String title;
+
+    @Column(name ="pretty_name", length = 50,nullable = false,unique = true)
+    private String prettyName;
+
+    @Column(name = "location",length = 255,nullable = false)
+    private String location;
+
+    @Column(name = "price",nullable = false)
+    private double price;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "endDate")
+    private LocalDate end_date;
+
+    @Column(name = "start_time")
+    private LocalTime startTime;
+
+    @Column(name = "end_time")
+    private LocalTime endTime;
+
+
+    //Precisa de getter e setter pois os atributos estão privados e precisam de acesso , posso usar o lombok
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrettyName() {
+        return prettyName;
+    }
+
+    public void setPrettyName(String prettyName) {
+        this.prettyName = prettyName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+}
+
+
